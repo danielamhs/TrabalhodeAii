273ea231e6404041c07d2ad0a9890728f875534a
danielamhs <a14349@esars.pt>
2024-11-05 08:56:36 +0000

    adicionei o comit com a lista de contas e as operações da subtração, multiplicação e divisão

--- calculadora.py
+++ calculadora.py
@@ -15,10 +15,31 @@ def divisao (x, y):
     return x / y
     
 # Função principal
- operador =input("Digite a operação: ")
+def calculadora():
+    lista_contas=[]
+    while True:
+        operador =input("Digite a operação: ")
         if operador == "soma":
             num1= int(input("digite o primeiro número: "))
             num2= int(input("digite o segundo número; "))
             result= soma(num1, num2)
             print("resultado da soma: " + str(result))
             lista_contas.append(str(num1) + " + " + str(num2) + " = "+ str(result))
+                elif operador == "sub":
+            num1= int(input("digite o  primeiro número: "))
+            num2= int(input("digite o segundo número"))
+            result= subtracao(num1, num2)
+            print("resultado da subtração: " + str(result))
+            lista_contas.append(str(num1) + " - " + str(num2) + "=" + str(result))
+        elif operador == "mult":
+            num1= int(input("digite o primeiro número: "))
+            num2= int(input("digite o segundo número: "))
+            result= multiplicacao(num1, num2)
+            print("resultado da multiplicação: " + str(result))
+            lista_contas.append(str(num1) + "*" + str(num2) + "=" + str(result))
+        elif operador == "div":
+            num1= int(input("digite o primeiro número: "))
+            num2= int(input("digite o segundo ´número: "))
+            result= divisao(num1, num2)
+            print("resultado da divisão: " + str(result))
+            lista_contas.append(str(num1) + "/" + str(num2) + "=" + str(result))
